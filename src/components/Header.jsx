7a94ad60f133d9f43196e15494188153c0f3d418
Smook-e <ahmednaser2382005@gmail.com>
2026-02-20 22:34:25 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,39 +3,74 @@ import { navigation } from '../constants';
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Button from './Button';
+import { HamburgerMenu } from './design/Header';
+import MenuSvg from '../assets/svg/MenuSvg';
 const Header = () => {
     const [activeLink, setActiveLink] = useState(null);
     const pathname = useLocation();
+    const [openNavigation, setOpenNavigation] = useState(true);
+
+    const toggleNavigation = () => {
+        setOpenNavigation((prev) => !prev);
+    }
+    const onclick = (id) => {
+        setActiveLink(id);
+        toggleNavigation();
+    }
     return (
-        <div className="fixed top-0 z-50 w-full bg-n-8/90 backdrop-blur-sm border-b border-n-6">
-            <div className="flex items-center px-5 lg:px-7-5 xl:px-10 max-lg:py-4">
-                <a href="#hero" className='block w-48 xl:mr-8'>
-                    <img src={brainwave} alt="BrainWave Logo" />
+        <div
+            className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+                }`}
+        >
+            <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+                <a className="block w-[12rem] xl:mr-8" href="#hero">
+                    <img src={brainwave} width={190} height={40} alt="Brainwave" />
                 </a>
 
-                <nav className='hidden fixed top-20 left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
-                    <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
+                <nav
+                    className={`${openNavigation ? "flex" : "hidden"
+                        } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex mx-auto lg:bg-transparent`}
+                >
+                    <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
                         {navigation.map((item) => (
-                            <a href={item.url} key={item.id} 
-                            className={`block relative font-code text-2xl  uppercase  transition-colors hover:text-color-1 ${pathname.hash === item.url ? 'lg:text-n-1' : 'lg:text-n-1/50'} lg:leading-5 xl:px-12 lg:hover:text-n-1 ${item.onlyMobile ? 'lg:hidden' : ''} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-sm `}
+                            <a
+                                key={item.id}
+                                href={item.url}
+                                onClick={() => onclick(item.id)}
+                                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? "lg:hidden" : ""
+                                    } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url === pathname.hash
+                                        ? "z-2 lg:text-n-1"
+                                        : "lg:text-n-1/50"
+                                    } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
                             >
                                 {item.title}
                             </a>
                         ))}
                     </div>
+
+                    <HamburgerMenu />
                 </nav>
-                <a href="#signup" className='button hidden mr-8 text-n-1/50 hover:text-n-1 transition-colors lg:block'>
-                        New account
+
+                <a
+                    href="#signup"
+                    className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+                >
+                    New account
                 </a>
-                <Button href={"#login"} className={"hidden lg:flex"}>
+                <Button className="hidden lg:flex " href="#login">
                     Sign in
                 </Button>
 
-
-                
+                <Button
+                    className="ml-auto lg:hidden"
+                    px="px-3"
+                    onClick={toggleNavigation}
+                >
+                    <MenuSvg openNavigation={openNavigation} />
+                </Button>
             </div>
         </div>
-    )
+    );
 }
 
 export default Header
